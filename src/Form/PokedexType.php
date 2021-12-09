34913286401b02c57ebda2ac59f9579174987503
Allandeveloppement92 <allan.germe@outlook.fr>
2021-12-09 17:26:54 +0100

    Mise à jour projet

--- src/Form/PokedexType.php
+++ src/Form/PokedexType.php
@@ -6,6 +6,8 @@ use App\Entity\Pokedex;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
 
 class PokedexType extends AbstractType
 {
